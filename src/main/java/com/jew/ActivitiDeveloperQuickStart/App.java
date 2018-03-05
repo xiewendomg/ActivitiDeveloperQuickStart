1410ff227b01f8d7a9bd317a0889df8009b75cb7
谢文东 <xiewendong@jew315.com>
2018-03-05 15:54:53 +0800

    Signed-off-by: 谢文东 <xiewendong@jew315.com>

---
+++ src/main/java/com/jew/ActivitiDeveloperQuickStart/App.java
@@ -0,0 +1,13 @@
+package com.jew.ActivitiDeveloperQuickStart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/jew/ActivitiDeveloperQuickStart/OnboardingRequest.java
@@ -0,0 +1,34 @@
+package com.jew.ActivitiDeveloperQuickStart;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;//Activiti流程引擎和配置
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+
+public class OnboardingRequest {
+
+	public static void main(String[] args) {
+		ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+		        .setJdbcUrl("jdbc:h2:mem:activiti;DB_CLOSE_DELAY=1000")
+		        .setJdbcUsername("sa")
+		        .setJdbcPassword("")
+		        .setJdbcDriver("org.h2.Driver")
+		        .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+		    ProcessEngine processEngine = cfg.buildProcessEngine();
+		    String pName = processEngine.getName();
+		    String ver = ProcessEngine.VERSION;
+		    System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
+
+		    RepositoryService repositoryService = processEngine.getRepositoryService();
+		    Deployment deployment = repositoryService.createDeployment()
+		        .addClasspathResource("onboarding.bpmn20.xml").deploy();
+		    ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+		        .deploymentId(deployment.getId()).singleResult();
+		    System.out.println(
+		        "Found process definition [" 
+		            + processDefinition.getName() + "] with id [" 
+		            + processDefinition.getId() + "]");
+		  }
+}
